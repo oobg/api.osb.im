a7db93fed8ec07c0d638ded0116ff1b76ea57625
bae woong <109705692+oobg@users.noreply.github.com>
2024-08-10 20:28:48 +0900

    feat: create github service
    Controller는 DB 관련된 로직을 처리한다.
    데이터 유효성을 체크하거나, DB에 아이템을 생성하는 등의 작업을 처리한다.
    
    명령어 : nest g service github --no-spec

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GithubController } from './github/github.controller';
+import { GithubService } from './github/github.service';
 
 @Module({
   imports: [],
   controllers: [AppController, GithubController],
-  providers: [AppService],
+  providers: [AppService, GithubService],
 })
 export class AppModule {}
