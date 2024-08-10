5694e9870510ccc9c44836d4419befd19b579866
bae woong <109705692+oobg@users.noreply.github.com>
2024-08-10 21:12:07 +0900

    feat: CRUD 구현하기 (Create)
    @Post 로 변경사항 수신받아 처리
    @Body 를 바로 쓸 수도 있다. 이 경우, 모든 데이터를 한번에 받을 수 있다.
    혹은 수신받을 데이터를 명시적으로 입력 가능하다.
    ID 값을 유니크 값으로 할당하기 위해 uudi 라이브러리를 설치했다.

--- src/github/github.controller.ts
+++ src/github/github.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from "@nestjs/common";
+import { Body, Controller, Get, Post } from "@nestjs/common";
 import { GithubService } from "./github.service";
 import { Board } from "./board.model";
 
@@ -9,5 +9,13 @@ export class GithubController {
 	@Get('/')
 	getAllBoards(): Board[] {
 		return this.githubService.getAllBoards();
+	}
+
+	@Post()
+	createBoard(
+		@Body('title') title: string,
+		@Body('description') description: string,
+	): Board {
+		return this.githubService.createBoard(title, description);
 	}
 }
