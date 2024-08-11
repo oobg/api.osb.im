29fa2c18bacabeb0b4762f5cddd47d83c3b6e5c0
bae woong <109705692+oobg@users.noreply.github.com>
2024-08-11 12:44:10 +0900

    feat: 특정 게시물을 찾을 때 없는 경우 결과 값 처리
    Nest.js Built-in 메서드인 NotFoundException 사용
    해당 메서드 내부에 텍스트를 삽입하면, 작성한 텍스트로 에러내용이 리턴된다.

--- src/github/github.service.ts
+++ src/github/github.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, NotFoundException } from "@nestjs/common";
 import { Board, BoardStatus } from "./board.model";
 import { v1 as uuid } from "uuid";
 import { CreateBoardDto } from "./dto/create-board.dto";
@@ -25,7 +25,13 @@ export class GithubService {
 	}
 
 	getBoardById(id: string): Board {
-		return this.boards.find((board) => board.id === id);
+		const found = this.boards.find((board) => board.id === id);
+
+		if (!found) {
+			throw new NotFoundException(`다음 ID를 찾을 수 없습니다. '${id}'`);
+		}
+
+		return found;
 	}
 
 	deleteBoard(id: string): void {
