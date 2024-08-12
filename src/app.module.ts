cd1baae3221e66b0a308efaad3115dd31be96530
bae woong <109705692+oobg@users.noreply.github.com>
2024-08-12 23:44:08 +0900

    refactor: boards.module 에 controller 와 service 삽입

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { BoardsController } from './boards/boards.controller';
-import { BoardsService } from './boards/boards.service';
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { TypeOrmConfig } from "./configs/typeorm.config";
 import { BoardsModule } from './boards/boards.module';
@@ -10,7 +8,5 @@ import { BoardsModule } from './boards/boards.module';
     TypeOrmModule.forRoot(TypeOrmConfig),
     BoardsModule,
   ],
-  controllers: [BoardsController],
-  providers: [BoardsService],
 })
 export class AppModule {}
