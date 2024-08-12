18dec7f1cfa992ab33cac865796ae7889288899a
bae woong <109705692+oobg@users.noreply.github.com>
2024-08-12 23:43:13 +0900

    fix: 이름 변경 Board → BoardEntity

--- src/boards/board.entity.ts
+++ src/boards/board.entity.ts
@@ -2,7 +2,7 @@ import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 import { BoardStatus } from "./board-status.enum";
 
 @Entity()
-export class Board extends BaseEntity {
+export class BoardEntity extends BaseEntity {
 	@PrimaryGeneratedColumn()
 	id: number;
 
