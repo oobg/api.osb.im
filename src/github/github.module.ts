016ebe3fd15f0d1fdb46c6073bc1ca8aeed866d6
bae woong <109705692+oobg@users.noreply.github.com>
2024-09-13 09:05:20 +0900

    feat: 정적 리소스 모둘 설정

--- src/github/github.module.ts
+++ src/github/github.module.ts
@@ -3,10 +3,17 @@ import { CustomModule } from "../configs/typeorm/custom.module";
 import { GithubController } from "./github.controller";
 import { GithubService } from "./github.service";
 import { GithubRepository } from "./github.repository";
+import { ServeStaticModule } from "@nestjs/serve-static";
+import { join } from "path";
+import "dotenv/config";
 
 @Module({
 	imports: [
-		CustomModule.forCustomRepository([GithubRepository])
+		CustomModule.forCustomRepository([GithubRepository]),
+		ServeStaticModule.forRoot({
+			rootPath: join(__dirname, "src", process.env.GIT_DIR || "clone"),  // 클론한 프로젝트의 경로
+			serveRoot: "/static",  // 리소스에 접근할 경로 설정
+		}),
 	],
 	controllers: [GithubController],
 	providers: [GithubService],
