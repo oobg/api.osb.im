f0364f9e32a37f8f4aec31c5a1d46c281f364244
bae woong <109705692+oobg@users.noreply.github.com>
2024-09-17 11:52:21 +0900

    feat: 정적 리소스 파일 가져오도록 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,8 @@ import { BoardsModule } from './boards/boards.module';
 import { ConfigModule } from "@nestjs/config";
 import { GithubModule } from './github/github.module';
 import { ScheduleModule } from "@nestjs/schedule";
+import { ServeStaticModule } from "@nestjs/serve-static";
+import { join } from "path";
 
 @Module({
   imports: [
@@ -13,6 +15,10 @@ import { ScheduleModule } from "@nestjs/schedule";
       envFilePath: [`./env/.env.${process.env.NODE_ENV}`],
       isGlobal: true,
       cache: true,
+    }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, "..", process.env.GIT_DIR),  // 클론한 프로젝트의 경로
+      serveRoot: "/file",  // 리소스에 접근할 경로 설정
     }),
     TypeOrmModule.forRootAsync({ useFactory: TypeOrmConfig }),
     ScheduleModule.forRoot(),
